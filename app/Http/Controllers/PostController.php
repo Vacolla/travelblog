185959161699ec6976403c4046d82f22e3f6a4a2
Vacolla <42672645+Vacolla@users.noreply.github.com>
2018-08-27 09:38:12 +0300

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,97 +4,140 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
-
+use Image;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+
+    public function index() //CRUD - Read
     {
-        //
+        //create a variable and store all the blog posts in it from the database
+        $posts = Post::all();
+
+        //return a view and pass in the above variable
+        return view('posts.index')->withPosts($posts);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+
+
+
+
+    public function create() //CRUD - Create
     {
-        return view('posts.create'); /* CRUD */
+        return view('posts.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
+
+
+
     public function store(Request $request) /*Post rules*/
     {
-      $this->validate($request, array(  /*verified CSRF*/
 
-     //validate data
+       //validate data
+       $this->validate($request, array(  /*verified CSRF*/
        'title' => 'required|max:255',
+       'slug' => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
        'body' => 'required'
      ));
 
      //store in the database
      $post = new Post;
      $post->title = $request->title;
+     $post->slug = $request->slug;
      $post->body = $request->body;
+
+    //save our image
+    if($request->hasFile('featured_image')){
+       $image = $request->file('featured_image');
+       $filename = time() . '.' . $image->getClientOriginalExtension();
+       $location = public_path('images/' . $filename);
+       Image::make($image)->resize(800, 400)->save($location);
+
+       $post->image = $filename;
+
+    }
+
      $post->save();
 
      return redirect()->route('posts.show', $post->id);
 
+
+
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
+
+
+
     public function show($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.show')->withPost($post);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
+
+
+
+
     public function edit($id)  /* CRUD */
     {
-        //
+        // find the post in the database and save as var
+        $post = Post::find($id);
+       // return the view and pass in the var
+       return view('posts.edit')->withPost($post);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
+
+
+
     public function update(Request $request, $id)  /* CRUD */
     {
-        //
+        // Validate the data
+        $post = Post::find($id);
+      if ($request->input('slug') == $post->slug) {
+        $this->validate($request, array(  /*verified CSRF*/
+        'title' => 'required|max:255',
+        'body' => 'required'
+      ));
+    } else {
+        $this->validate($request, array(  /*verified CSRF*/
+        'title' => 'required|max:255',
+        'slug' => 'required|alpha_dash|min:5|max:255|unique:posts,slug',
+        'body' => 'required'
+      ));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+        // Save the data to the database
+         $post = Post::find($id);
+
+         $post->title = $request->input('title');
+         $post->slug = $request->input('slug');
+         $post->body = $request->input('body');
+
+         $post->save();
+
+        //Redirect with flashdata to the posts.show
+
+        return redirect()->route('posts.show', $post->id);
+    }
+
+
+
+
+
+
+
     public function destroy($id)    /* CRUD */
     {
-        //
+
+      $post = Post::find($id);
+
+      $post->delete();
+
+      return redirect()->route('posts.index');
+
     }
 }
