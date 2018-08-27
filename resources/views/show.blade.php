63f1d8c392e609f239c86704c5ef5cfa0cefe2b4
Vacolla <42672645+Vacolla@users.noreply.github.com>
2018-08-27 09:37:00 +0300

    Create show.blade.php

---
+++ resources/views/show.blade.php
@@ -0,0 +1,54 @@
+@extends('main')
+
+<title>View Post</title>
+
+@section ('content')
+
+  <div class="row">
+    <div class="col-md-8">
+       <h1>{{ $post->title }}</h1>
+
+     <p class="lead">{{ $post->body }}</p>
+   </div>
+
+   <div class="col-md-4">
+    <div class="well">
+      <dl class="dl-horizontal">
+        <label>URL:</label>
+      <p> <a href="{{ url('blog/'.$post->slug) }}">{{ url('blog/'.$post->slug) }}</a></p>
+      </dl>
+
+     <dl class="dl-horizontal">
+       <label>Created At:</label>
+       <p>{{ $post->created_at }}</p>
+     </dl>
+
+     <dl class="dl-horizontal">
+       <label>Last Update:</label>
+       <p>{{ $post->updated_at }}</p>
+     </dl>
+<hr>
+
+     <div class="row">
+       <div class="col-sm-6">
+         {!! Html::linkRoute('posts.edit', 'Edit', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
+       </div>
+       <div class="col-sm-6">
+         {{ method_field('PUT') }}
+         {!! Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'DELETE']) !!}
+         {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-block']) !!}
+
+         {!! Form::close() !!}
+
+  <div class="row">
+    <div class="col-md-12">
+      {{ Html::linkRoute('posts.index', 'See More', [], ['class' => 'btn btn-default btn-block btn-h1-spacing'])}}
+
+    </div>
+   </div>
+ </div>
+</div>
+</div>
+
+
+@endsection
